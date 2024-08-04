4e4ce0e3f6b1fc1eedbc533527a36678ded8faed
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-08-04 10:32:07 +0530

    Version-1

---
+++ Dougs Dog Doors/Models/DogDoor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dougs_Dog_Doors.Models
+{
+    internal class DogDoor
+    {
+        private bool open;
+
+        public DogDoor()
+        {
+            open = false;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("Door is open");
+            open = true;
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Door is close");
+            open = false;
+        }
+
+        public bool IsOpen()
+        {
+            return open;
+        }
+
+    }
+}
---
+++ Dougs Dog Doors/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dougs_Dog_Doors.Models;
+
+namespace Dougs_Dog_Doors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DogDoor door = new DogDoor();
+            Remote remote = new Remote(door);
+
+            Console.WriteLine("Fido barks to go outside...........");
+            remote.PressButton();
+            Console.WriteLine("\n Fido has gone Outside.......");
+            remote.PressButton();
+            Console.WriteLine("\n Fido's all done");
+            remote.PressButton();
+            Console.WriteLine("\n Fido's back inside");
+            remote.PressButton();
+        }
+    }
+}
